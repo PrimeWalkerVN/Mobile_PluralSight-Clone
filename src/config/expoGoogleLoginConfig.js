3a5a751aa1b7fc44448279bdbda8a86f7f1d2716
PrimeWalkerVN <nchithanh609@gmail.com>
2021-01-21 21:24:38 +0700

    Google for deploy app android

--- src/config/expoGoogleLoginConfig.js
+++ src/config/expoGoogleLoginConfig.js
@@ -1,5 +1,5 @@
 const expoGoogleLoginConfig = {
   iosClientId: '68967187307-c3p6aaili8t9d43aet2h09h5d2qfp3o8.apps.googleusercontent.com',
-  androidClientId: `68967187307-7js92coi0mctodc9ma6r01kuqnnhdje4.apps.googleusercontent.com`,
+  androidClientId: `68967187307-ugrtqh91cb12p840rt7jcviaepfbpm5v.apps.googleusercontent.com`,
 };
 export default expoGoogleLoginConfig;
